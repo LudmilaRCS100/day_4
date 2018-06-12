8a85dc06a5a464110603a49ed7adc3b8ab30d737
Ludmila <luvi_lv@inbox.lv>
2018-06-12 21:08:47 +0300

    step1

---
+++ main.js
@@ -0,0 +1,30 @@
+document.querySelector('#btn1').addEventListener('click', function() {
+    var someString1='foo';//string
+    var someString2='baz';
+})
+document.querySelector('#btn2').addEventListener('click', function() {
+    var someString1='foo';//string
+    var someString2='baz';
+})
+document.querySelector('#btn3').addEventListener('click', function() {
+    var someString1='foo';//string
+    var someString2='baz';
+})
+    
+    if ('#btn1'==""){
+        output = 'foo'
+        
+    }
+    
+   
+    if ('#btn2'==""){
+        output = 'baz'
+        console.log(output);
+    }
+    if ('#btn3'==""){
+        output = 'baz'
+        console.log(output);
+    }
+
+    
+
